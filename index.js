e73e193c1bdc8e99531397651f56a9e0268583f1
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-06-12 15:00:24 -0400

    Added start game button css, html & JS

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ let hasBlackjack = false;
 let isAlive = true;
 // create meassage to inform the person if they won or lost
 let message = '';
-// create if/else statements
-if (sum <= 21) {
-	message = 'Do you a want to draw a new card?';
-} else if (sum === 21) {
-	message = "Wohoo! You've got Blackjack!";
-	hasBlackjack = true;
-} else {
-	message = "You're out of the game!";
-	isAlive = false;
+// Create startGame() function
+function startGame() {
+	// create if/else statements
+	if (sum <= 21) {
+		message = 'Do you a want to draw a new card?';
+	} else if (sum === 21) {
+		message = "Wohoo! You've got Blackjack!";
+		hasBlackjack = true;
+	} else {
+		message = "You're out of the game!";
+		isAlive = false;
+	}
+	console.log(message);
 }
 // print to the HTML
