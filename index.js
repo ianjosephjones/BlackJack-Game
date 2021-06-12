e5e3562f05ec933e90187d60f42eccad19e9c889
ianjosephjones <64451104+ianjosephjones@users.noreply.github.com>
2021-06-12 13:47:24 -0400

    Create outline for blackjack game

---
+++ index.js
@@ -0,0 +1,15 @@
+// Variables to for cards
+let firstCard = 10;
+let secondCard = 9;
+// total cards drawn
+let sum = firstCard + secondCard
+// boolean if the person won or now
+let hasBlackjack = false;
+let isAlive = true;
+// create meassage to inform the person if they won or lost
+let message = '';
+// create if/else statements
+if (sum <= 21) {
+
+}
+// print to the HTML
